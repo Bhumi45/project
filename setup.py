7686639fdd12d706b6247bd3c35e0b3c293d487c
Bhumi45 <bhumika@parihar.in>
2024-11-09 12:39:05 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,25 @@
+# it consist of a code with which we are creatin package as we will deploy our poject as package.
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    # this function will return the list of requirements
+    requirements=[]
+    with open("requirements.txt") as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup( # this is the formation of package
+    name="Fabric_Detection_Project",
+    version="0.0.1",
+    author="Bhumika Parihar",
+    author_email="bhumikaparihar03@gmail.com",
+    packages=find_packages(), # it will find the subpackages inside this folder.
+    install_requires=get_requirements("requirements.txt")
+)
